5bfdf7c2f4eee7c43a41946842e207f8643b74c7
Derya K <96728812+deryakmrt@users.noreply.github.com>
2023-07-14 15:36:24 +0300

    mükemmel sayi

---
+++ deneme28.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class deneme28 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int sayi,toplam=0;
+        System.out.println("Sayı giriniz:");
+        sayi=input.nextInt();
+        for(int i=1;i<sayi;i++){
+            if(sayi%i==0){
+                toplam=toplam+i;
+            }
+        }if(toplam==sayi){
+            System.out.println(sayi+" Mükemmel sayıdır.");
+        }else {
+            System.out.println(sayi+" Mükemmel sayı değildir.");
+        }
+    }
+}
